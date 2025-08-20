da4962e7616355d469d04ccf71644130eab85c6b
Jackson Wallace <jwallace1410@gmail.com>
2025-08-19 21:21:11 -0400

    style: update status line style

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,14 @@
 return {
-  "vague2k/vague.nvim",
-  config = function()
-    require("vague").setup({
-      transparent = true,
-    })
-    vim.cmd.colorscheme("vague")
-  end,
+	"vague2k/vague.nvim",
+	config = function()
+		require("vague").setup({
+			transparent = true,
+		})
+		vim.cmd.colorscheme("vague")
+
+		vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+
+		vim.opt.statusline = "%t"
+	end,
 }
