3197b9d0282cb9fc9904766546c02907ee3dc414
Jackson Wallace <jwallace1410@gmail.com>
2025-08-21 14:42:27 -0400

    feat: add modified icon in status line nvim

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,14 +1,14 @@
 return {
-	"vague2k/vague.nvim",
-	config = function()
-		require("vague").setup({
-			transparent = true,
-		})
-		vim.cmd.colorscheme("vague")
+  "vague2k/vague.nvim",
+  config = function()
+    require("vague").setup({
+      transparent = true,
+    })
+    vim.cmd.colorscheme("vague")
 
-		vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
-		vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
 
-		vim.opt.statusline = "%t"
-	end,
+    vim.opt.statusline = "%t %{&modified?'+':''}"
+  end,
 }
