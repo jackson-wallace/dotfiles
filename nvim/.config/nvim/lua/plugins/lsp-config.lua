7f1068e34885b2a6cba78d9ed6aec664fee62f82
Jackson Wallace <jwallace1410@gmail.com>
2025-11-28 14:11:37 -0600

    change leader ws to leader fs for symbol search in nvim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -67,7 +67,7 @@ return {
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
       vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+      vim.keymap.set("n", "<leader>fs", vim.lsp.buf.workspace_symbol, opts)
       vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
       vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
       vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
